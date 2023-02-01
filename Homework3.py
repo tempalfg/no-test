5778e91f9fab8e9d8c6df8c85b482f1cea71e094
tempa <tempaprsd@gmail.com>
2023-02-01 23:01:43 +0300

    first commit

---
+++ Homework3.py
@@ -0,0 +1,79 @@
+#Задание 1
+#Пользователь вводит с клавиатуры номер дня недели
+#(1-7). Необходимо вывести на экран названия дня недели. Например, если 1, то на экране надпись понедельник,
+#2 — вторник и т.д.
+
+#number = int(input("Введите номер дня недели (1-7) :"))
+#if number == 1:
+#    print("Понедельник")
+#elif number == 2:
+#    print("Вторник")
+#elif number == 3:
+#    print("Среда")
+#elif number == 4:
+#    print("Четверг")
+#elif number == 5:
+#    print("Пятница")
+#elif number == 6:
+#    print("Суббота")
+#else:
+#    print("Воскресенье")
+
+#Задание 2
+#Пользователь вводит с клавиатуры номер месяца
+#(1-12). Необходимо вывести на экран название месяца.
+#Например, если 1, то на экране надпись январь, 2 — февраль и т.д.
+
+#month = int(input("Введите номер месяца (1-12) : "))
+#if month == 1:
+#    print("Январь")
+#elif month == 2:
+#    print("Февраль")
+#elif month == 3:
+#    print("Март")
+#elif month == 4:
+#    print("Апрель")
+#elif month == 5:
+#    print("Май")
+#elif month == 6:
+#    print("Июнь")
+#elif month == 7:
+#    print("Июль")
+#elif month == 8:
+#    print("Август")
+#elif month == 9:
+#    print("Сентябрь")
+#elif month == 10:
+#    print("Октябрь")
+#elif month == 11:
+#    print("Ноябрь")
+#elif month == 12:
+#    print("Декабрь")
+#else:
+#    print("Ваш ввод не имеет , введите число от 1 до 12")
+
+#Задание 3
+#Пользователь вводит с клавиатуры число. Если число
+#больше нуля нужно вывести надпись «Number is positive»,
+#если меньше нуля «Number is negative», если равно нулю
+#«Number is equal to zero»
+#numbers = int(input("Введите число:"))
+#if numbers > 0:
+#    print("Number is positive")
+#elif numbers < 0:
+#    print("Number is negative")
+#else:
+#    print("Number is equal to zero")
+
+#Задание 4
+#Пользователь вводит два числа. Определить, равны
+#ли эти числа, и, если нет, вывести их на экран в порядке
+#возрастания.
+
+#numbers1 = int(input("Введите первое число: "))
+#numbers2 = int(input("Введите второе число: "))
+#if numbers1 == numbers2:
+#    print("Числа равны")
+#else:
+#    print(min([numbers1,numbers2]),max([numbers1,numbers2])) #подсмотрел решение в интернете
+
---
+++ Homework6.py
@@ -0,0 +1,50 @@
+#Задание 1
+#Напишите программу, которая запрашивает два
+#целых числа x и y, после чего вычисляет и выводит
+#значение x в степени y.
+
+#x = int(input('Введите первое число: '))
+#y = int(input('Введите второе число: '))
+#degree = x ** y
+#print(degree)
+
+#Задание 2
+#Подсчитать количество целых чисел в диапазоне от
+#100 до 999 у которых есть две одинаковые цифры.
+#count = 0
+#for i in range(100, 1000):
+#    first = i // 100
+#    second = i // 10 % 10
+#    third = i % 10
+#    if (first == second and first != third) or (first == third and first != second) or (second == third and second != first):
+#        count += 1
+#print(count)
+
+#адание 3
+#подсчитать количество целых чисел в диапазоне от
+#100 до 9999 у которых все цифры разные.
+#count1 = 0
+#count2 = 0
+#for i in range (1000, 10000):
+#    first = i // 1000
+#    second = i // 100 % 10
+#    third = i % 100 // 10
+#    fourth = i % 10
+#    if first != second and first != third and first != fourth and second != third and second != fourth and third != fourth:
+#        count1 += 1
+#for i in range (100, 1000):
+#    first = i // 100
+#    second = i // 10 % 10
+#    third = i % 10
+#    if first != second and first != third and second != third:
+#        count2 += 1
+#print(count1 + count2)
+
+#Задание 4
+#Пользователь вводит любое целое число. Необходимо из этого целого числа удалить все цифры 3 и 6 и
+#вывести обратно на экран.
+
+
+
+
+
